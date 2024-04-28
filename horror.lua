728c59584602edd8d41a179b423daf091cf5214a
Cvxxrni <134775994+Cvxxrni@users.noreply.github.com>
2024-04-28 09:29:11 +0000

    Update horror.lua

--- horror.lua
+++ horror.lua
@@ -1,4 +1,4 @@
-_G.thingtosay = "ADD MY DESKO Q.SK FOR CHEATS AND FOLLOW ON BLUE AND BLACK APP LEGITRLPRO " 
+_G.thingtosay = "Q.SK IS ONTOP GET ARES INSTEAD OF SIRIUS ADD DESKO Q.SK " 
 if not _G.shoption then _G.shoption = "largest" end
 wait(5) if not game:IsLoaded() then game.Loaded:Wait() end
 
